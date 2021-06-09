696a795b56afe50e2ceb94b8f7bcb38a5e24a410
lihbr <lihbr@users.noreply.github.com>
2021-06-09 13:30:01 +0200

    docs: begin writing docs

---
+++ components/DicoLoginCli.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="prose dark:prose-dark">
+		<div class="nuxt-content-highlight">
+			<pre
+				class="language-bash line-numbers"
+			><code>dico login {{token}}</code></pre>
+			<d-copy-button />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			token: "<token>"
+		};
+	},
+	mounted() {
+		this.maybeGetToken();
+	},
+	methods: {
+		async maybeGetToken() {
+			const response = await fetch(`https://api.dico.app/v1/manage/token/pat`, {
+				method: "GET",
+				credentials: "include"
+			});
+
+			if (!response.ok) {
+				if (response.status !== 401) {
+					console.error(response);
+				}
+			} else {
+				const json = await response.json();
+
+				if (json.data.token) {
+					this.token = json.data.token;
+				} else {
+					console.error(json);
+				}
+			}
+		}
+	}
+};
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,17 @@ import { withDocus } from "docus";
 
 export default withDocus({
 	rootDir: __dirname,
+	css: [
+		"@fontsource/inter/300.css",
+		"@fontsource/inter/400.css",
+		"@fontsource/inter/600.css",
+		"@fontsource/inter/700.css",
+		"@fontsource/inter/800.css",
+		"@fontsource/manrope/800.css",
+		"@fontsource/manrope/700.css",
+		"@fontsource/manrope/600.css",
+		"@fontsource/inconsolata/600.css"
+	],
 	plugins: ["~/plugins/gtag.client"],
 	generate: {
 		fallback: true
