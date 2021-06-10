bd5d43453e53931c8df48d6d356678edbe6718ac
lihbr <lihbr@users.noreply.github.com>
2021-06-10 10:49:41 +0200

    feat: update header depending on auth state

--- components/DSocialIcons.vue
+++ components/DSocialIcons.vue
@@ -4,8 +4,27 @@
 			Back to dico.app ->
 		</dico-button>
 		<dico-button
+			v-if="!isLoggedIn"
 			as="a"
 			href="https://ui.dico.app"
+			theme="reversed-primary"
+			class="hidden sm:block"
+		>
+			Login
+		</dico-button>
+		<dico-button
+			v-if="!isLoggedIn"
+			as="a"
+			href="https://ui.dico.app"
+			theme="primary"
+			class="hidden sm:block"
+		>
+			Signup
+		</dico-button>
+		<dico-button
+			v-if="isLoggedIn"
+			as="a"
+			href="https://ui.dico.app/dico"
 			theme="primary"
 			class="hidden sm:block"
 		>
@@ -15,7 +34,38 @@
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			isLoggedIn: false
+		};
+	},
+	mounted() {
+		this.checkIsLoggedIn();
+	},
+	methods: {
+		async checkIsLoggedIn() {
+			const response = await fetch(`https://api.dico.app/v1/auth/whoami`, {
+				method: "GET",
+				credentials: "include"
+			});
+
+			if (!response.ok) {
+				if (response.status !== 401) {
+					console.error(response);
+				}
+			} else {
+				const json = await response.json();
+
+				if (json.status === 200) {
+					this.isLoggedIn = true;
+				} else {
+					console.error(json);
+				}
+			}
+		}
+	}
+};
 </script>
 
 <style>
